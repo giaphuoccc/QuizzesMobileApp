c85aadd4e074727eaac29a93431251ecc8590bd0
Hau2223 <nguyentrunghau220203@gmail.com>
2024-06-11 13:12:51 +0700

    completed conect with NodeJs vs Library vector icons

--- Screen/123.js
+++ Screen/123.js
@@ -1,14 +1,19 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const homeScreen = () => {
   return (
     <View>
       <Text>34444</Text>
+      <Icon 
+      name="people"
+      size={30}
+      color="#900"/>
     </View>
-  )
-}
+  );
+};
 
-export default homeScreen
+export default homeScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
