2b51b21aa03d087db988fb5005451d4875d98717
Hau2223 <nguyentrunghau220203@gmail.com>
2024-06-15 13:52:40 +0700

    Loading & Welcome Screen

--- StackNavigator.js
+++ StackNavigator.js
@@ -1,27 +1,49 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
 import RankingScreen from './Screen/rankingScreen';
 import GreetingScreen from './Screen/greetingScreen';
 import BriefScreen from './Screen/briefScreen';
-
+import Loading from './Screen/LoadingScreen.js';
+import Welcome from './Screen/WelcomeScreen.js';
 
 const StackNav = createNativeStackNavigator();
 
 const stack = () => {
   return (
     <NavigationContainer>
-      <StackNav.Navigator initialRouteName='GreetingScreen'>
-        <StackNav.Screen name="RankingScreen" component={RankingScreen} options={{headerShown: false}} />
-        <StackNav.Screen name="GreetingScreen" component={GreetingScreen} options={{headerShown: false}} />
-        <StackNav.Screen name="BriefScreen" component={BriefScreen} options={{headerShown: false}} />
-
+      <StackNav.Navigator initialRouteName="Loading">
+        <StackNav.Screen
+          name="Loading"
+          component={Loading}
+          options={{headerShown: false}}
+        />
+        <StackNav.Screen
+          name="Welcome"
+          component={Welcome}
+          options={{headerShown: false}}
+        />
+        <StackNav.Screen
+          name="GreetingScreen"
+          component={GreetingScreen}
+          options={{headerShown: false}}
+        />
+        <StackNav.Screen
+          name="RankingScreen"
+          component={RankingScreen}
+          options={{headerShown: false}}
+        />
+        <StackNav.Screen
+          name="BriefScreen"
+          component={BriefScreen}
+          options={{headerShown: false}}
+        />
       </StackNav.Navigator>
     </NavigationContainer>
-  )
-}
+  );
+};
 
-export default stack
+export default stack;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
