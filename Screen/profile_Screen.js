74c9a3987d67adf7ed5748e797515a4baf8d519c
Bearyeiz <giaphuoc.work@gmail.com>
2024-06-15 13:26:46 +0700

    add Screen

--- Screen/profile_Screen.js
+++ Screen/profile_Screen.js
@@ -1,31 +1,70 @@
 import React from 'react';
 import { View, Image, Text } from 'react-native';
-
 //import avatar from './path/to/your/local/avatar.jpg';
 
-const YourComponent = () => {
+const ProfileScreen = () => {
   return (
     <View style={{ height: '100%', flexDirection: 'column' }}>
       {/* White with avatar */}
       <View style={{ flex: 0.25, backgroundColor: '#FFFFFF', justifyContent: 'center', alignItems: 'center' }}>
         <Image
-          //source={avatar} //
+          //source={avatar}
           source={require('../Assets/Images/avatar_profile.png')} // Replace with your avatar URL
-          style={{ width: 250, height: 200, borderRadius: 150 }} // Adjust the size as needed
+          style={{ width: 150, height: 150, borderRadius: 75 }}
         />
       </View>
 
       {/* Blue with achievement */}
-      <View style={{ flex: 0.75, backgroundColor: '#086CA4' }}>
-        <Text style={{ color: '#FFFFFF', fontSize: 72, fontWeight: 'bold', position: 'absolute', top: 10, left: 15 }}>
+      <View style={{ flex: 0.75, backgroundColor: '#086CA4', padding: 10 }}>
+        <Text style={{ color: '#FFFFFF', fontSize: 32, fontWeight: 'bold'}}>
           Your Name
         </Text>
-        <Text style={{ color: '#FFFFFF', fontSize: 72, fontWeight: 'bold', position: 'absolute', top: 10, left: 15 }}>
-          @Username
-        </Text>
+        <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
+          {/* Phần tử thứ nhất */}
+          <View style={{ flex: 1, alignItems: 'flex-start' }}>
+            <Text style={{ color: '#FFFFFF', fontSize: 16 }}>
+              @username
+            </Text>
+          </View>
+
+          {/* Phần tử thứ hai */}
+          <View style={{ flex: 1, alignItems: 'flex-start' }}>
+            <Text style={{ color: '#FFFFFF', fontSize: 16 }}>
+              Joined: DD/MM/YYYY
+            </Text>
+          </View>
+        </View>
+
+
+        {/* Folowers và Follow */}
+        <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center', marginTop: 15 }}>
+          {/* Followers */}
+          <View style={{ flex: 1, alignItems: 'flex-start' }}>
+            <Text style={{ color: '#FFFFFF', fontWeight: 'bold', fontSize: 32 }}>
+              0
+            </Text>
+            <Text style={{ color: '#FFFFFF', fontSize: 16 }}>
+              Người theo dõi
+            </Text>
+          </View>
+
+          <View style={{ height: '100%', width: 1, backgroundColor: '#FFFFFF', marginHorizontal: 10 }} />
+          
+          {/* Follow */}
+          <View style={{ flex: 1, alignItems: 'flex-start' }}>
+            <Text style={{ color: '#FFFFFF', fontWeight: 'bold', fontSize: 32 }}>
+              0
+            </Text>
+            <Text style={{ color: '#FFFFFF', fontSize: 16 }}>
+              Đang theo dõi
+            </Text>
+          </View>
+        </View>
+        
+        
       </View>
     </View>
   );
 };
 
-export default YourComponent;
+export default ProfileScreen;
