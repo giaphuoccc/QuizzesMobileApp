0e8b6b6825916ba87db44381baabcfa227844407
TriiTonyy1708 <135950372+TriiTonyy1708@users.noreply.github.com>
2024-06-20 15:36:28 +0700

    update UI homeScreen

--- Screen/homeScreen.js
+++ Screen/homeScreen.js
@@ -1,13 +1,13 @@
-import { StyleSheet, Text, View, ImageBackground } from 'react-native'
+import { StyleSheet, Text, View, ImageBackground, TouchableOpacity } from 'react-native'
 import React from 'react'
 import * as Progress from 'react-native-progress';
 import Icon from 'react-native-vector-icons/Octicons';
 
 
-const HomeScreen = ({navigation}) => {
+const HomeScreen = ({ navigation }) => {
     return (
         <View style={{ height: '100%', flexDirection: 'column' }}>
-            {/* <View style={[styles.topic]}>
+            <View style={[styles.topic]}>
                 <View style={[styles.topicContainer]}>
                     <View style={[styles.topicText]}>
                         <Text style={[styles.topicName]}>Chapter I</Text>
@@ -27,19 +27,46 @@ const HomeScreen = ({navigation}) => {
                 </View>
             </View>
             <View style={[styles.content]} >
-            <Icon name="check-circle-fill" style={[styles.icon_1]} onPress={()=>navigation.navigate("QuizHolderScreen")}/>
+                <View style={[styles.iconContainer_16_1]}>
+                    <TouchableOpacity style={[styles.iconBackground_1]} onPress={() => navigation.navigate("QuizHolderScreen")}>
+                        <Icon name="check-circle-fill" style={[styles.icon]} />
+                    </TouchableOpacity>
+                </View>
+
                 <View style={[styles.layout]}>
                     <ImageBackground resizeMode="contain" style={[styles.image]} source={require('../Assets/Images/man.png')} />
                     <View style={[styles.iconLayout]}>
-                        <Icon name="feed-star" style={[styles.icon_2]} />
-                        <Icon name="x-circle-fill" style={[styles.icon_34]} />
-                        <Icon name="x-circle-fill" style={[styles.icon_34]} />
-                        <Icon name="feed-star" style={[styles.icon_5]} />
+                        <View style={[styles.iconContainer_25_1]}>
+                            <TouchableOpacity style={[styles.iconBackground_2]}>
+                                <Icon name="feed-star" style={[styles.icon]} />
+                            </TouchableOpacity>
+                        </View>
+                        <View style={[styles.iconContainer_34_1]}>
+                            <View style={[styles.iconContainer]}>
+                                <TouchableOpacity style={[styles.iconBackground_34]}>
+                                    <Icon name="x-circle-fill" style={[styles.icon]} />
+                                </TouchableOpacity>
+                            </View>
+                            <View style={[styles.iconContainer]}>
+                                <TouchableOpacity style={[styles.iconBackground_34]}>
+                                    <Icon name="x-circle-fill" style={[styles.icon]} />
+                                </TouchableOpacity>
+                            </View>
+                        </View>
+                        <View style={[styles.iconContainer_25_1]}>
+                            <TouchableOpacity style={[styles.iconBackground_5]}>
+                                <Icon name="x-circle-fill" style={[styles.icon]} />
+                            </TouchableOpacity>
+                        </View>
                     </View>
                 </View>
-                <Icon name="check-circle-fill" style={[styles.icon_6]} />
-            </View> */}
-            <View style={[styles.topic]}>
+                <View style={[styles.iconContainer_16_1]}>
+                    <TouchableOpacity style={[styles.iconBackground_6]}>
+                        <Icon name="x-circle-fill" style={[styles.icon]} />
+                    </TouchableOpacity>
+                </View>
+            </View>
+            {/* <View style={[styles.topic]}>
                 <View style={[styles.topicContainer]}>
                     <View style={[styles.topicText]}>
                         <Text style={[styles.topicName]}>Chapter II</Text>
@@ -59,18 +86,45 @@ const HomeScreen = ({navigation}) => {
                 </View>
             </View>
             <View style={[styles.content]} >
-                <Icon name="check-circle-fill" style={[styles.icon_1]} onPress={()=>navigation.navigate("QuizHolderScreen")}/>
+                <View style={[styles.iconContainer_16_2]}>
+                    <TouchableOpacity style={[styles.iconBackground_1]} onPress={() => navigation.navigate("QuizHolderScreen")}>
+                        <Icon name="check-circle-fill" style={[styles.icon]} />
+                    </TouchableOpacity>
+                </View>
+
                 <View style={[styles.layout]}>
                     <View style={[styles.iconLayout]}>
-                        <Icon name="feed-star" style={[styles.icon_2]} />
-                        <Icon name="x-circle-fill" style={[styles.icon_34_2]} />
-                        <Icon name="x-circle-fill" style={[styles.icon_34_2]} />
-                        <Icon name="feed-star" style={[styles.icon_5]} />
+                        <View style={[styles.iconContainer_25_2]}>
+                            <TouchableOpacity style={[styles.iconBackground_2]}>
+                                <Icon name="feed-star" style={[styles.icon]} />
+                            </TouchableOpacity>
+                        </View>
+                        <View style={[styles.iconContainer_34_2]}>
+                            <View style={[styles.iconContainer]}>
+                                <TouchableOpacity style={[styles.iconBackground_34]}>
+                                    <Icon name="x-circle-fill" style={[styles.icon]} />
+                                </TouchableOpacity>
+                            </View>
+                            <View style={[styles.iconContainer]}>
+                                <TouchableOpacity style={[styles.iconBackground_34]}>
+                                    <Icon name="x-circle-fill" style={[styles.icon]} />
+                                </TouchableOpacity>
+                            </View>
+                        </View>
+                        <View style={[styles.iconContainer_25_2]}>
+                            <TouchableOpacity style={[styles.iconBackground_5]}>
+                                <Icon name="x-circle-fill" style={[styles.icon]} />
+                            </TouchableOpacity>
+                        </View>
                     </View>
-                    <ImageBackground style={[styles.image_2]} source={require('../Assets/Images/man.png')} />
+                    <ImageBackground resizeMode="contain" style={[styles.image_2]} source={require('../Assets/Images/man.png')} />
                 </View>
-                <Icon name="check-circle-fill" style={[styles.icon_6]} />
-            </View>
+                <View style={[styles.iconContainer_16_2]}>
+                    <TouchableOpacity style={[styles.iconBackground_6]}>
+                        <Icon name="x-circle-fill" style={[styles.icon]} />
+                    </TouchableOpacity>
+                </View>
+            </View> */}
         </View>
     )
 }
@@ -79,7 +133,7 @@ export default HomeScreen
 
 const styles = StyleSheet.create({
     topic: {
-        flex: 2,
+        flex: 1,
         backgroundColor: '#2C3C67',
     },
     topicContainer: {
@@ -117,23 +171,114 @@ const styles = StyleSheet.create({
         fontWeight: 'bold'
     },
     content: {
-        flex: 12,
+        flex: 5,
         backgroundColor: '#2E4583',
-        paddingHorizontal: '5%'
+        paddingHorizontal: '10%',
+        paddingVertical:'5%'
+
+    },
+    iconContainer_16_1: {
+        width:'55%',
+        flex: 1,
+        alignItems: 'flex-end',
+        justifyContent: 'center',
+    },
+    iconContainer_25_1: {
+        width:'20%',
+        flex: 1,
+        alignItems: 'flex-end',
+        justifyContent: 'center',
+    },
+    iconContainer_16_2: {
+        width:'65%',
+        flex: 1,
+        alignItems: 'flex-end',
+        justifyContent: 'center',
+    },
+    iconContainer_25_2: {
+        width:'45%',
+        flex: 1,
+        alignItems: 'flex-end',
+        justifyContent: 'center',
+    },
+    iconContainer: {
+        flex: 2,
+        alignItems: 'center',
+        justifyContent: 'center',
+        // backgroundColor:'red'
+    },
+    iconContainer_34_1: {
+        flex: 2,
+        width:'55%',
+        flexDirection:'column',
+        // backgroundColor: 'red',
+        justifyContent:'center',
+        alignSelf: 'flex-end',  
+    },
+    iconContainer_34_2: {
+        flex: 2,
+        width:'60%',
+        flexDirection:'column',
+        // backgroundColor: 'red',
+        justifyContent:'center',
+        alignSelf: 'flex-start',  
+    },
+    iconBackground_1:
+    {
+        backgroundColor: 'green',
+        width: 100,
+        height: 100,
+        borderRadius: 50,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    iconBackground_2:
+    {
+        backgroundColor: 'yellow',
+        width: 100,
+        height: 100,
+        borderRadius: 50,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    iconBackground_34:
+    {
+        backgroundColor: 'red',
+        width: 100,
+        height: 100,
+        borderRadius: 50,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    iconBackground_5:
+    {
+        width: 100,
+        height: 100,
+        borderRadius: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'gray',
+    },
+    iconBackground_6:
+    {
+        width: 100,
+        height: 100,
+        borderRadius: 50,
+        backgroundColor: 'gray',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    icon: {
+        fontSize: 85,
+        color: 'white',
     },
     icon_1: {
         flex: 1,
         fontSize: 100,
-        textAlignVertical: 'center',
-        textAlign: 'center',
-        color: '#61FF00'
+        color: '#61FF00',
     },
     icon_2: {
-        flex: 2,
-        fontSize: 100,
-        textAlignVertical: 'center',
-        textAlign: 'center',
-        color: 'yellow',
+
     },
     layout: {
         flex: 4,
@@ -141,47 +286,21 @@ const styles = StyleSheet.create({
         alignItems: 'center',
     },
     image: {
-        flex: 1.5,
+        flex: 2,
         height: 200,
         width: 200,
         // backgroundColor:'red'
     },
-    image_2:{
+    image_2: {
         flex: 1.5,
         height: 200,
         width: 200,
-        transform:'scaleX(-1);'
+        transform: 'scaleX(-1);'
     },
     iconLayout: {
-        flex: 2,
-    },
-    icon_34: {
-        flex: 2,
-        fontSize: 100,
-        textAlignVertical: 'center',
-        textAlign: 'right',
-        color: 'red',
-    },
-    icon_34_2:{
-        flex: 2,
-        fontSize: 100,
-        textAlignVertical: 'center',
-        textAlign: 'left',
-        color: 'red',
-    },
-    icon_5: {
-        flex: 2,
-        fontSize: 100,
-        textAlignVertical: 'center',
-        textAlign: 'center',
-        color: '#EEEEEE',
-    },
-    icon_6: {
-        flex: 1,
-        fontSize: 100,
-        textAlignVertical: 'center',
-        textAlign: 'center',
-        color: '#EEEEEE'
+        flex: 3,
+        alignItems: 'center',
+        justifyContent: 'space-evenly',
     },
     NavBar: {
         flex: 1
